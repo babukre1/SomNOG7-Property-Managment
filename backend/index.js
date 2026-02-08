2a5dd4ce50442c739d4747522950dfda114ac9ae
Abubakr <abubakrwindowz@gmail.com>
2026-02-08 20:09:24 +0300

    updated origin to match frontend URL

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "propertymanagmentfrontend.vercel.app" }));
+app.use(cors({ origin: "https://propertymanagmentfrontend.vercel.app" }));
 
 // Connect to the database
 Dbconnect();
